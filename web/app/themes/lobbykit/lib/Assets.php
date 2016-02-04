462316c7e46d5b02c600890cb177c3d807a835ab
Andreas Ek <andreas@andreasek.se>
2016-02-04 09:24:18 -0500

    Applied fixes from StyleCI

--- web/app/themes/lobbykit/lib/Assets.php
+++ web/app/themes/lobbykit/lib/Assets.php
@@ -1,31 +1,32 @@
 <?php
+
 namespace LobbyKit\Intra;
 
-class Assets {
+class Assets
+{
+    public function __construct()
+    {
+        add_action('wp_enqueue_scripts', '\LobbyKit\Intra\Assets::enqueueScripts');
 
-	public function __construct() {
-		add_action( 'wp_enqueue_scripts', '\LobbyKit\Intra\Assets::enqueueScripts' );
+        add_action('init', function () {
+        });
+    }
 
-		add_action( 'init', function() {
-		});
+    public static function get($file)
+    {
+        return get_stylesheet_directory_uri().'/dist/'.$file;
+    }
 
-	}
+    public static function enqueueScripts()
+    {
+        $theme = wp_get_theme();
+        $version = $theme->version;
 
-	static function get($file) {
-		return get_stylesheet_directory_uri().'/dist/'.$file;
-	}
-
-	static function enqueueScripts() {
-
-		$theme = wp_get_theme();
-		$version = $theme->version;
-
-		wp_enqueue_style( 'modular-admin-vendor-css', assets('css/vendor.css'), [], $version );
-		wp_enqueue_style( 'modular-admin-css', assets('css/app.css'), [], $version );
-		wp_enqueue_script( 'modular-admin-vendor-js', assets('js/vendor.js'), [], $version, true );
-		wp_enqueue_script( 'modular-admin-js', assets('js/app.js'), [], $version, true );
-	}
-	
+        wp_enqueue_style('modular-admin-vendor-css', assets('css/vendor.css'), [], $version);
+        wp_enqueue_style('modular-admin-css', assets('css/app.css'), [], $version);
+        wp_enqueue_script('modular-admin-vendor-js', assets('js/vendor.js'), [], $version, true);
+        wp_enqueue_script('modular-admin-js', assets('js/app.js'), [], $version, true);
+    }
 }
 
 new Assets();
