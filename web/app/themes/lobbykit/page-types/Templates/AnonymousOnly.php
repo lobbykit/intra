f1f8bdc87593f04fd7f59452cb7cb17413c3548d
Andreas Ek <andreas@andreasek.se>
2016-02-06 09:45:26 -0500

    Applied fixes from StyleCI

--- web/app/themes/lobbykit/page-types/Templates/AnonymousOnly.php
+++ web/app/themes/lobbykit/page-types/Templates/AnonymousOnly.php
@@ -1,13 +1,14 @@
 <?php
+
 return [
-	'title'       => __('Authorization','intra'),
-	'context'     => 'side',
-	'priority'     => 'default',
-	papi_property([
-		'slug'        => 'anonymous_only',
-		'title'       => __('Anonymous only', 'intra'),
-		'description' => __('Check this if only non logged in users should see this module', 'intra'),
-		'type'        => 'bool',
-	]),
+    'title'        => __('Authorization', 'intra'),
+    'context'      => 'side',
+    'priority'     => 'default',
+    papi_property([
+        'slug'        => 'anonymous_only',
+        'title'       => __('Anonymous only', 'intra'),
+        'description' => __('Check this if only non logged in users should see this module', 'intra'),
+        'type'        => 'bool',
+    ]),
 
 ];
